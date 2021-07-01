e1274feeeb730272317578aa5c0130a9a2be70b1
Ryan Safner <ryansafner@gmail.com>
2021-07-01 12:18:20 -0400

    Add game theory

---
+++ scripts/gametheory.R
@@ -0,0 +1,5 @@
+library(hexSticker)
+
+law_img <- "image_sources/game_theory.pdf"
+sticker(law_img, package="", h_color="#6A5ACD", h_fill="#FFFFFF", s_x=1, s_y=1, s_width=0.65,
+        filename="game_theory.png")
