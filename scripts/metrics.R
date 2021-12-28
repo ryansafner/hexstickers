bf739dd292f1dfc9f7529b0d7ae8d5ac097edb2e
Ryan Safner <ryansafner@gmail.com>
2021-12-28 13:41:03 -0700

    Update

--- scripts/metrics.R
+++ scripts/metrics.R
@@ -20,12 +20,13 @@ p<-ggplot(data = gapminder2007,
           mapping = aes(x=gdpPercap,
                         y=lifeExp))+
   geom_point(aes(fill=continent, size=pop), color="white", pch=21)+
-  geom_smooth(color="black", se=F)+
+  #geom_smooth(color="#170C3AFF", se=F)+
   #geom_point(aes(size=pop),shape=1,color="white")+
   theme_void()+
   scale_x_log10(labels=scales::dollar)+
   theme(legend.position = "")+
-  scale_size(breaks = c(100000, 1000000, 100000000, 1000000000), labels=c("<1 million","1-100 million","100-999 million", "1 billion+"), range=c(1,15))
+  scale_size(breaks = c(100000, 1000000, 100000000, 1000000000), labels=c("<1 million","1-100 million","100-999 million", "1 billion+"), range=c(1,15))+
+  scale_fill_viridis_d(option = "magma", begin = 0.3, end = 0.9)
 
-sticker(p, package="", h_color="#236192", h_fill="#FFFFFF", s_x=1, s_y=.9, s_width=1.6, s_height=1.35,
+sticker(p, package="", h_color="#314f4f", h_fill="#FFFFFF", s_x=1, s_y=.9, s_width=1.6, s_height=1.35,
         filename="stickers_png/metrics_hex.png")
