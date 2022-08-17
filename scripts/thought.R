622ea0758e9d963124822a734884ce19d3490ca2
Ryan Safner <ryansafner@gmail.com>
2022-08-16 21:07:24 -0400

    Fix thought hex

--- scripts/thought.R
+++ scripts/thought.R
@@ -1,5 +1,5 @@
 library(hexSticker)
 
-het_img <- "image_sources/History_of_Thought_v6.pdf"
-sticker(het_img, package="", h_color="#b14848", h_fill="#FFFFFF", s_x=0.96, s_y=1.05, s_width=1.2,
+het_img <- "image_sources/History_of_Thought_v7.pdf"
+sticker(het_img, package="", h_color="#b14848", h_fill="#FFFFFF", s_x=1.05, s_y=1.05, s_width=0.75,
         filename="stickers_png/thought_hex.png")
